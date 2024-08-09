e335f7d9c215efa9f7865d84227decd97a6013ca
durgesh <durgeshhirani131@gmail.com>
2024-08-09 14:50:37 +0530

    add to github

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+@section('title', 'Home Page - Online Store')
+@section('subtitle', 'Hirani Retail')
+@section('content')
+    <div class="text-center">
+        Welcome to the application
+    </div>
+@endsection
+
+// this is working fine.
